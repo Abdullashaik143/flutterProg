3e81119886dd94e045416665a02b4fc336e0ef4a
Abdulla Shaik <Abdullashaik62@yahoo.com>
2019-02-17 01:07:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+void main( )
+{
+  runApp(new MyApp());
+}
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home:new Center(
+        child: new Text("hi Abdulla How do you Do.....?"),
+      ),
+    );
+  }
+}
